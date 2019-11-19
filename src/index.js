8b8bad292cb3fc61b5df23ab6ba4fa9eb946f530
DmitriyChuprin <32386707+DmitriyChuprin@users.noreply.github.com>
2019-11-19 23:54:54 +0300

    add li

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 const el = (
   <div>
     <h1>My TODO List</h1>
-    <input placeholder="search"/>
+    <input placeholder="search" />
     <ul>
       <li>Learn React</li>
       <li>Build Awesome App</li>
+      <li>Go to sleep</li>
     </ul>
-    </div>
-  );
+  </div>
+);
 
-ReactDOM.render(el, 
-  document.getElementById('root'));
+ReactDOM.render(el, document.getElementById("root"));
