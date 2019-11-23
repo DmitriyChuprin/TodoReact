7cc9e01f8ce6879ac0d2a9279603bc3b6adbc5c5
DmitriyChuprin <chuprin-dmitriy@mail.ru>
2019-11-23 15:18:34 +0300

    change some files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My TODO List</h1>
-    <input placeholder="search"/>
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
+import ItemStatusFilter from './components/item-status-filter';
+
+import './index.css';
+
+const App = () => {
+
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
+
+  return (
+    <div className="todo-app">
+      <AppHeader toDo={1} done={3} />
+      <div className="top-panel d-flex">
+        <SearchPanel />
+        <ItemStatusFilter />
+      </div>
+
+      <TodoList todos={todoData} />
     </div>
   );
+};
 
-ReactDOM.render(el, 
+ReactDOM.render(<App />,
   document.getElementById('root'));
