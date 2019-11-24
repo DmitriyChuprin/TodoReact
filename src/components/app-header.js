92d91e848fac8a3024099f1f5c4f4b322d6bcc1b
DmitriyChuprin <32386707+DmitriyChuprin@users.noreply.github.com>
2019-11-24 16:06:47 +0300

    add

--- src/components/app-header.js
+++ src/components/app-header.js
@@ -1,23 +1,15 @@
-<<<<<<< HEAD
-import React from 'react';
-import './app-header.css';
+import React from "react";
+import "./app-header.css";
 
-const AppHeader = ({toDo, done}) => {
+const AppHeader = ({ toDo, done }) => {
   return (
     <div className="app-header d-flex">
       <h1>Todo List</h1>
-      <h2>{toDo} more to do, {done} done</h2>
+      <h2>
+        {toDo} more to do, {done} done
+      </h2>
     </div>
   );
 };
 
 export default AppHeader;
-=======
-import React from "react";
-
-const AppHead = () => {
-  return <h1>My Todo List</h1>;
-};
-
-export default AppHead;
->>>>>>> 83527d46e14b95654475c7e8b62f5376d7e626ad
--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
-import React from 'react';
+import React from "react";
 
-import './search-panel.css';
+import "./search-panel.css";
 
 const SearchPanel = () => {
   return (
-    <input type="text"
-              className="form-control search-input"
-              placeholder="type to search" />
+    <input
+      type="text"
+      className="form-control search-input"
+      placeholder="type to search"
+    />
   );
-=======
-import React from "react";
-
-const SearchPanel = () => {
-  const searchText = "Type here to search";
-  const searchStyle = {
-    fontSize: "20px"
-  };
-  return <input style={searchStyle} placeholder={searchText} />;
->>>>>>> 83527d46e14b95654475c7e8b62f5376d7e626ad
 };
 
 export default SearchPanel;
